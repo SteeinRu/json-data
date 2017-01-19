e7dcd91960b908f8e5d8e4b04168afb2927cc4ae
Steein Inc <support@steein.ru>
2017-01-20 02:02:25 +0300

    variation in the test

--- build/index.php
+++ build/index.php
@@ -6,12 +6,9 @@ include_once '../vendor/autoload.php';
 use SteeinJSON\JSON;
 
 
+
 $json = new JSON();
 
 $json->test->one->two = 's';
 
 echo $json->getObject('test')->getObject('one')->getObject('two');
-
-
-
-//echo $json;
